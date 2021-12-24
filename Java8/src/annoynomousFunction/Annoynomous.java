ccf81c76622aca4a55ad6b5f7c1cb7b7d8240962
Abdul Abid <Abdul Abid@AbdulAbid>
2021-12-24 07:35:41 +0530

    functional interface

--- Java8/src/annoynomousFunction/Annoynomous.java
+++ Java8/src/annoynomousFunction/Annoynomous.java
@@ -1,7 +1,28 @@
 package annoynomousFunction;
 
-
+interface Calculator {
+	abstract void sum();
+	//	abstract void sum1(); An Interface that contains exactly one abstract method is known as functional interface
+}
 
 public class Annoynomous {
 
+//	@Override
+//	public void sum() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void sum1() {
+//		// TODO Auto-generated method stub
+//		
+//	}  methods are need to be implemented using interface to avoid to implementing all methods we can use annoynomous method 
+	
+	public static void  main(String args[]) {
+Calculator calculator=()->{System.out.println("welcome");};
+ 
+calculator.sum();
+	
+}
 }
